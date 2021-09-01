8436da4e749cc24ae31b8170cbc1939ba86326b3
Marcos Mantovani <mantovani.marcos@gmail.com>
2021-09-01 11:44:21 -0300

    added webpack for js processing/generation; added edge dev tools launch json for js development

---
+++ src/app.js
@@ -0,0 +1,15 @@
+class Calendar {
+
+  contructor(height, width) {
+    this.height = height;
+    this.width = width;
+    this.render()
+  }
+
+  render() {
+    return this
+  }
+
+}
+
+new Calendar(5, 5)
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "js")
+  },
+  mode: "production",
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
